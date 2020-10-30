cdb80a8d1fcf914d0c499dbe6637eecdafde162d
Max <maxs987@gmail.com>
2020-10-30 15:33:26 +0300

    INHERIT_ALL in __init__

--- connectome/__init__.py
+++ connectome/__init__.py
@@ -1,2 +1,3 @@
 from .interface.base import Source, Transform, Chain
 from .interface.blocks import CacheToRam, CacheToDisk, CacheRows, Apply, RemoteStorage, Merge
+from .layers.base import INHERIT_ALL
