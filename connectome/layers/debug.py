80deea70c072f55bc23bb3a52e835b1cb1efa697
Max <maxs987@gmail.com>
2021-01-19 13:43:16 +0300

    added a layer for cache invalidation debug

---
+++ connectome/layers/debug.py
@@ -0,0 +1,24 @@
+from typing import Iterable, Sequence
+
+from .transform import TransformLayer, INHERIT_ALL
+from ..cache.disk import key_to_relative
+from ..engine.base import Node, NodeHash, NodesMask
+from ..engine.edges import IdentityEdge
+
+
+class HashDigest(IdentityEdge):
+    def _evaluate(self, arguments: Sequence, mask: NodesMask, node_hash: NodeHash):
+        pickled, digest, _ = key_to_relative(node_hash.value)
+        return arguments[0], node_hash.value, digest, pickled
+
+
+class HashDigestLayer(TransformLayer):
+    def __init__(self, names: Iterable[str]):
+        inputs, outputs, edges = [], [], []
+        for name in names:
+            inp, out = Node(name), Node(name)
+            inputs.append(inp)
+            outputs.append(out)
+            edges.append(HashDigest().bind(inp, out))
+
+        super().__init__(inputs, outputs, edges, inherit_nodes=INHERIT_ALL)
