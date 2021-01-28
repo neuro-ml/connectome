a1e575cea7d597505c03cca4854989a3c9bb3691
Max <maxs987@gmail.com>
2021-01-28 20:03:20 +0300

    groupby in `__init__`

--- connectome/interface/__init__.py
+++ connectome/interface/__init__.py
@@ -1,3 +1,3 @@
 from .base import Source, Transform, Chain, chained
-from .blocks import CacheToRam, CacheToDisk, CacheRows, Apply, RemoteStorage, Merge, Filter
+from .blocks import CacheToRam, CacheToDisk, CacheRows, Apply, RemoteStorage, Merge, Filter, GroupBy
 from .decorators import insert, optional, inverse, positional
