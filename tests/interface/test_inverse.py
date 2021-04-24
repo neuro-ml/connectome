bde0b08977b6802e4c64700f4eda04c8736daf8c
Max <maxs987@gmail.com>
2021-04-24 20:06:57 +0300

    found a new cool bug with loopback

---
+++ tests/interface/test_inverse.py
@@ -0,0 +1,47 @@
+from connectome import Transform, inverse
+
+
+def test_different_inputs():
+    class A(Transform):
+        __inherit__ = True
+
+        def _y(y):
+            return y
+
+        @inverse
+        def output(output, _y):
+            return output
+
+    class B(Transform):
+        def x(x, y):
+            return x + y
+
+        @inverse
+        def output(output):
+            return output
+
+    dec = (A() >> B())._decorate('x', 'output')
+
+    @dec
+    def func(x):
+        return x
+
+    assert func(x=1, y=2) == 3
+
+
+def test_missing_input():
+    class EraseBeyondLungs(Transform):
+        __inherit__ = True
+
+        def _param(param):
+            return param
+
+        @inverse
+        def output(output, _param):
+            return output, _param
+
+    dec = EraseBeyondLungs()._decorate('input', 'output')
+
+    @dec
+    def predict(x):
+        return 1, 2
