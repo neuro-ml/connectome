2793fee88fa2c24a4e77f31de0962f3290a98ffb
Max <maxs987@gmail.com>
2021-06-27 19:16:05 +0300

    added redis to tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 pytest_plugins = ['graph_fixtures', 'interface_fixtures', 'disk_fixtures']
@@ -6,4 +8,6 @@ markers = ['redis']
 
 @pytest.fixture
 def redis_hostname():
+    if os.environ.get('CI'):
+        return 'redis'
     return 'localhost'
