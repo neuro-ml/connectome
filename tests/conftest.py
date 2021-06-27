de256c32fc16e03ecf38e9a17b185466aec32ced
Max <maxs987@gmail.com>
2021-06-27 19:19:37 +0300

    fixed hostname

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 
 pytest_plugins = ['graph_fixtures', 'interface_fixtures', 'disk_fixtures']
@@ -8,6 +6,4 @@ markers = ['redis']
 
 @pytest.fixture
 def redis_hostname():
-    if os.environ.get('CI'):
-        return 'redis'
     return 'localhost'
