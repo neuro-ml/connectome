af266a6db2899e9655c83f114102b291f07cc2df
Max <maxs987@gmail.com>
2021-07-03 13:03:17 +0300

    updated setup

--- connectome/__init__.py
+++ connectome/__init__.py
@@ -1,2 +1,3 @@
 from .interface import *
 from .containers import INHERIT_ALL
+from .__version__ import __version__
--- setup.py
+++ setup.py
@@ -1,14 +1,34 @@
+import runpy
+from pathlib import Path
+
 from setuptools import setup, find_packages
 
-with open('requirements.txt', encoding='utf-8') as file:
+classifiers = '''Development Status :: 3 - Alpha
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9'''
+
+name = 'connectome'
+root = Path(__file__).parent
+with open(root / 'requirements.txt', encoding='utf-8') as file:
     requirements = file.read().splitlines()
+with open(root / 'README.md', encoding='utf-8') as file:
+    long_description = file.read()
+version = runpy.run_path(root / name / '__version__.py')['__version__']
 
 setup(
-    name='connectome',
-    packages=find_packages(include=('connectome',)),
+    name=name,
+    packages=find_packages(include=(name,)),
     include_package_data=True,
-    version='0.0.1',
-    keywords=[],
+    version=version,
+    description='A library for datasets containing heterogeneous data',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/neuro-ml/connectome',
+    download_url='https://github.com/neuro-ml/connectome/archive/v%s.tar.gz' % version,
+    keywords=['dag', 'dataset', 'cache', 'consistency'],
+    classifiers=classifiers.splitlines(),
     install_requires=requirements,
     python_requires='>=3.6',
 )
