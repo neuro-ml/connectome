abef0dc39cc17cab99cc4aab6a197ada1d23cd01
Max <maxs987@gmail.com>
2021-10-25 00:42:05 +0300

    fixed root

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).parent))
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
 
 # -- Project information -----------------------------------------------------
 
