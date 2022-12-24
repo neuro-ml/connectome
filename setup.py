92997aaf4197becbb2668daab969aeabcfc5e5a6
Max <maxs987@gmail.com>
2022-12-24 23:01:06 +0300

    fixed ubuntu version

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from pathlib import Path
 
 from setuptools import setup, find_packages
 
-classifiers = '''Development Status :: 3 - Alpha
-Programming Language :: Python :: 3.6
-Programming Language :: Python :: 3.7
-Programming Language :: Python :: 3.8
-Programming Language :: Python :: 3.9'''
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+]
 
 name = 'connectome'
 root = Path(__file__).parent
@@ -28,7 +31,7 @@ setup(
     url='https://github.com/neuro-ml/connectome',
     download_url='https://github.com/neuro-ml/connectome/archive/v%s.tar.gz' % version,
     keywords=['dag', 'dataset', 'cache', 'consistency'],
-    classifiers=classifiers.splitlines(),
+    classifiers=classifiers,
     install_requires=requirements,
     python_requires='>=3.6',
 )
