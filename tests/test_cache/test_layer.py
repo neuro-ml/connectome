6a0216b21a05170baac17aef902f847e6de83274
Max <maxs987@gmail.com>
2023-01-07 21:27:17 +0300

    fixed cache inheritance

---
+++ tests/test_cache/test_layer.py
@@ -0,0 +1,17 @@
+from connectome import Transform, CacheToRam
+
+
+def test_nested_virtual():
+    a = Transform(
+        a=lambda x: x,
+        b=lambda x: x,
+        c=lambda x: x,
+    )
+    b = Transform(
+        __inherit__=True,
+        d=lambda a: a,
+    )
+    c = CacheToRam('d')
+
+    assert set(dir(a >> b >> c)) == set('abcd')
+    assert set(dir(a >> (b >> c))) == set('abcd')
