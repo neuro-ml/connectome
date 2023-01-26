c173b7529a14fb66d61284670aa196811341ac22
Max <maxs987@gmail.com>
2023-01-26 18:45:16 +0400

    fix

--- scripts/check-reqs-consistency.py
+++ scripts/check-reqs-consistency.py
@@ -2,9 +2,9 @@ import tomllib
 from pathlib import Path
 
 root = Path(__file__).resolve().parent.parent
-with open(root / 'requirements.txt') as file:
+with open(root / 'requirements.txt', 'r') as file:
     first = set(filter(None, (x.strip() for x in file.readlines())))
-with open(root / 'pyproject.toml') as file:
+with open(root / 'pyproject.toml', 'rb') as file:
     second = set(tomllib.load(file)['project']['dependencies'])
 
 if first != second:
